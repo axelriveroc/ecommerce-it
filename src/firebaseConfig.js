54b8e41d53ad16fd2d237ba975c7c70fecab7307
axelriveroc <riverocoronelaxel@gmail.com>
2023-07-05 11:31:38 -0300

    se agrega firebase para auth y db

---
+++ src/firebaseConfig.js
@@ -0,0 +1,48 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+const auth = getAuth(app);
+
+// LOGIN
+    export const login = async({email, password})=>{
+        try {
+            let res = await signInWithEmailAndPassword( auth , email, password  )
+            return res
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+// REGISTER
+    export const register = async({email, password})=>{
+        try {
+            let res = await createUserWithEmailAndPassword(auth, email, password)
+            return res
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+
+// LOGIN WITH GOOGLE
+
+    export const loginWithGoogle = ()=>{}
+    
+    
