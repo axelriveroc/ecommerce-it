46f3174123cf99287ba7939fcb4fb6132602796b
axelriveroc <riverocoronelaxel@gmail.com>
2023-07-16 16:13:12 -0300

    modifique taare

--- tarea.js
+++ tarea.js
@@ -1,7 +1,5 @@
  "Manejar el input file en register de usuarios y pasarlo correctamente a la DB y la validacion formik. "
 
-"Hacer el pasamanos del usuario loggeado cuando ingresa con facebook, lo mismo que hice con ingresar con email y contraseña"
-
 "Agregar la funcionalidad de crear un nuevo producto"
 
 "Añadir una página para sumar opciones de editado que no entran en el modal"
